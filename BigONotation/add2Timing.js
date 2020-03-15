8c05976f30312daf4c93d7a2ae4c0c0303d86534
Pratik1399 <pratik.ghadge20@gmail.com>
2020-03-15 22:17:49 +0530

    Big O Notation

--- BigONotation/add2Timing.js
+++ BigONotation/add2Timing.js
@@ -1,5 +1,6 @@
+//add faster
 function addUpTo(n) {
-  return (n * (n + 1)) / 2;
+	return (n * (n + 1)) / 2;
 }
 
 let time1 = performance.now();
@@ -8,4 +9,4 @@ addUpTo(1000000000);
 
 let time2 = performance.now();
 
-console.log(`Time Elapsed:${(time2 - time1) / 1000}seconds.`);
+console.log(`Time Elapsed:${(time2 - time1) / 1000} seconds.`);
---
+++ BigONotation/logUpTo.js
@@ -0,0 +1,7 @@
+function logUpTo(n) {
+	for (var i = 1; i <= n; i++) {
+		console.log(i);
+	}
+}
+
+console.log(1);
